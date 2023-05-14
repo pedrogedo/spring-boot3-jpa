3488c83183c8836282c6e361c2b7c72ca4f8da93
Pedro Henrique Rosendo da Silva <pedro.ads@otlook.com>
2023-05-14 19:25:47 -0300

    User insert

--- src/main/java/br/com/phrosendo/course/controllers/UserController.java
+++ src/main/java/br/com/phrosendo/course/controllers/UserController.java
@@ -1,13 +1,17 @@
 package br.com.phrosendo.course.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.phrosendo.course.entities.User;
 import br.com.phrosendo.course.services.UserService;
@@ -30,4 +34,13 @@ public class UserController {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
+	
+	@PostMapping("/insert")
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 }
